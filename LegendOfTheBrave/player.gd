f71fe68553a6b5aff63778a0f99696f7a9b5ecec
CoffeeKiller <18320210361@163.com>
2024-03-23 15:52:16 +0800

    feat : add slide and optimize jump

--- LegendOfTheBrave/player.gd
+++ LegendOfTheBrave/player.gd
@@ -22,6 +22,9 @@ enum State {
 	ATTACK_3,	 # 三段攻击
 	HURT,		 # 受击
 	DYING,		 # 濒死
+	SLIDING_START, # 滑铲开始
+	SLIDING_LOOP,  # 滑铲过程
+	SLIDING_END,   # 滑铲结束
 }
 
 # GROUND_STATES 状态分组, 该组内的状态都是处于地面状态的
@@ -41,64 +44,38 @@ const JUMP_VELOCITY := -300.0
 const WALL_JUMP_VELOCITY := Vector2(380, -280) 
 # 常量, 击退力度, 默认:512
 const KNOCKBACK_AMOUNT := 512.0
+# 玩家滑铲滑动默认时间
+const SLIDING_DURATION := 0.3
+# 玩家滑铲时的加速度
+const SLIDING_SPEED := 240.0
+# 进入landing状态所需的高度
+const LANDING_HEIGHT := 80.0
+
 
 # can_combo 表示玩家当前是否触发combo(连击)
 @export var can_combo := false
+
 
 # 获取重力加速度 (通过项目设置获取)
 var default_gravity := ProjectSettings.get("physics/2d/default_gravity") as float
 var is_first_tick := false # 触犯判断, 用于限制无限跳跃
 var is_combo_requested := false  # 连击判断, 判断当前是否触发连击
 var pending_damage: Damage # 待处理的伤害
+var fall_from_y : float # 玩家掉落时的高度
+
 
 # 获取实体
 @onready var graphics: Node2D = $Graphics
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 @onready var coyote_timer: Timer = $CoyoteTimer
 @onready var jump_request_timer: Timer = $JumpRequestTimer
+@onready var slide_request_timer: Timer = $SlideRequestTimer
 @onready var invincible_timer: Timer = $InvincibleTimer # 受击无敌时间 计时器
 @onready var hand_checker: RayCast2D = $Graphics/HandChecker
 @onready var foot_checker: RayCast2D = $Graphics/FootChecker
 @onready var state_machin: Node = $StateMachin
 @onready var stats: Node = $Stats
 
-
-# 检测玩家输入
-func _unhandled_input(event: InputEvent) -> void:
-	# 为跳跃提供一个时机
-	if event.is_action_pressed("jump"):
-		jump_request_timer.start()
-	
-	# 根据空格按下的时间长短来决定跳跃的高度
-	if event.is_action_released("jump") and velocity.y < JUMP_VELOCITY / 2:
-		jump_request_timer.stop()
-		# 判断玩家空格恢复时 高度是否达到JUMP_VELOCITY的一半, 
-		# 如果没有就直接减少跳跃高度,提前结束跳跃
-		if velocity.y < JUMP_VELOCITY / 2:
-			velocity.y = JUMP_VELOCITY / 2 
-	
-	# 判断是否按下攻击并且在 combo 时机内
-	if event.is_action_pressed("attack") and can_combo:
-		is_combo_requested = true
-		
-
-# func _physics_process(delta: float) -> void:
-# 	# 获取玩家键盘输入
-# 	var direction := Input.get_axis("move_left", "move_right")
-# 	# 控制加速 地面的加速度 与 空中的加速度是不一样的.
-# 	var acceleration := FLOOR_ACCELRATION if is_on_floor() else AIR_ACCELRATION
-# 	# 为角色运动添加: 加速度系数
-# 	velocity.x = move_toward(velocity.x, direction * RUN_SPEED, acceleration * delta) 
-# 	velocity.y += gravity * delta
-#	# 设置郊狼时间 让角色在浮空的一定时间内也可以完成跳跃操作
-#		var can_jump := is_on_floor() or coyote_timer.time_left > 0
-#		var should_jump := can_jump and jump_request_timer.time_left > 0
-#		if should_jump:
-#			velocity.y = JUMP_VELOCITY
-
-# can_wall_slide 用于检测当前的墙面是否可以滑行
-func can_wall_slide() -> bool:
-	return is_on_wall() and hand_checker.is_colliding() and foot_checker.is_colliding()
 
 ## 物理帧处理函数, 物理帧更新时实际处理的相关逻辑
 func tick_physics(state: State, delta: float) -> void:
@@ -146,8 +123,15 @@ func tick_physics(state: State, delta: float) -> void:
 		State.HURT, State.DYING:
 			stand(default_gravity, delta)
 			
+		State.SLIDING_END:
+			stand(default_gravity, delta)
+			
+		State.SLIDING_LOOP, State.SLIDING_START:
+			slide(delta)
+			
 	is_first_tick = false
 	
+# 玩家移动处理函数	
 func move(gravity: float, delta: float) -> void:
 	# 获取玩家键盘输入
 	var direction := Input.get_axis("move_left", "move_right")
@@ -163,6 +147,7 @@ func move(gravity: float, delta: float) -> void:
 	
 	move_and_slide()
 	
+# 玩家站立位置处理函数	
 func stand(gravity:float, delta: float) -> void:
 	
 	# 控制加速 地面的加速度 与 空中的加速度是不一样的.
@@ -172,10 +157,64 @@ func stand(gravity:float, delta: float) -> void:
 	velocity.y += gravity * delta
 	
 	move_and_slide()
+
+# 玩家滑铲处理函数
+func slide(delte: float) -> void:
+	velocity.x = graphics.scale.x * SLIDING_SPEED
+	velocity.y += default_gravity * delte
 	
+	move_and_slide()
+
 # 玩家死亡函数
 func die() -> void:
 	get_tree().reload_current_scene()
+	
+# 检测玩家输入
+func _unhandled_input(event: InputEvent) -> void:
+	# 为跳跃提供一个时机
+	if event.is_action_pressed("jump"):
+		jump_request_timer.start()
+	
+	# 根据空格按下的时间长短来决定跳跃的高度
+	if event.is_action_released("jump") and velocity.y < JUMP_VELOCITY / 2:
+		jump_request_timer.stop()
+		# 判断玩家空格恢复时 高度是否达到JUMP_VELOCITY的一半, 
+		# 如果没有就直接减少跳跃高度,提前结束跳跃
+		if velocity.y < JUMP_VELOCITY / 2:
+			velocity.y = JUMP_VELOCITY / 2 
+	
+	# 判断是否按下攻击并且在 combo 时机内
+	if event.is_action_pressed("attack") and can_combo:
+		is_combo_requested = true
+	
+	# 判断当前是否按下滑铲键, 并启动 滑铲定时器
+	if event.is_action_pressed("slide"):
+		slide_request_timer.start()
+		
+
+# func _physics_process(delta: float) -> void:
+# 	# 获取玩家键盘输入
+# 	var direction := Input.get_axis("move_left", "move_right")
+# 	# 控制加速 地面的加速度 与 空中的加速度是不一样的.
+# 	var acceleration := FLOOR_ACCELRATION if is_on_floor() else AIR_ACCELRATION
+# 	# 为角色运动添加: 加速度系数
+# 	velocity.x = move_toward(velocity.x, direction * RUN_SPEED, acceleration * delta) 
+# 	velocity.y += gravity * delta
+#	# 设置郊狼时间 让角色在浮空的一定时间内也可以完成跳跃操作
+#		var can_jump := is_on_floor() or coyote_timer.time_left > 0
+#		var should_jump := can_jump and jump_request_timer.time_left > 0
+#		if should_jump:
+#			velocity.y = JUMP_VELOCITY
+
+# can_wall_slide 用于检测当前的墙面是否可以滑行
+func can_wall_slide() -> bool:
+	return is_on_wall() and hand_checker.is_colliding() and foot_checker.is_colliding()
+
+# should_slide 判断玩家当前位置是否可以进行滑铲
+func should_slide() -> bool:
+	if slide_request_timer.is_stopped():
+		return false
+	return not foot_checker.is_colliding()
 	
 # 切换下一个状态 (自动调用)
 func get_next_state(state: State) -> int:
@@ -201,12 +240,16 @@ func get_next_state(state: State) -> int:
 		State.IDLE:
 			if Input.is_action_just_pressed("attack"):
 				return State.ATTACK_1
+			if should_slide():
+				return State.SLIDING_START
 			if not is_still:
 				return State.RUNNING
 			
 		State.RUNNING:
 			if Input.is_action_just_pressed("attack"):
 				return State.ATTACK_1
+			if should_slide():
+				return State.SLIDING_START
 			if is_still:
 				return State.IDLE
 			
@@ -216,13 +259,12 @@ func get_next_state(state: State) -> int:
 			
 		State.FALL:
 			if is_on_floor():
-				return State.LANDING if is_still else State.RUNNING
+				var h := global_position.y - fall_from_y
+				return State.LANDING if h >= LANDING_HEIGHT else State.RUNNING
 			if can_wall_slide():
 				return State.WALL_SLIDING
 				
 		State.LANDING:
-			if not is_still:
-				return State.RUNNING
 			if not animation_player.is_playing():
 				return State.IDLE
 				
@@ -257,6 +299,18 @@ func get_next_state(state: State) -> int:
 		State.HURT:
 			if not animation_player.is_playing():
 				return State.IDLE
+				
+		State.SLIDING_START:
+			if not animation_player.is_playing():
+				return State.SLIDING_LOOP
+				
+		State.SLIDING_END:
+			if not animation_player.is_playing():
+				return State.IDLE
+		
+		State.SLIDING_LOOP:
+			if state_machin.state_time > SLIDING_DURATION or is_on_wall():
+				return State.SLIDING_END
 		
 	
 	return StateMachin.KEEP_CURRENT		
@@ -294,6 +348,7 @@ func transition_state(from: State, to: State) -> void:
 			animation_player.play("fall")
 			if from in GROUND_STATES:
 				coyote_timer.start()
+			fall_from_y = global_position.y
 		
 		State.LANDING:
 			animation_player.play("landing")
@@ -337,6 +392,19 @@ func transition_state(from: State, to: State) -> void:
 			animation_player.play("die")
 			# 死亡时不需要无敌时间了
 			invincible_timer.stop()
+			
+		State.SLIDING_START:
+			animation_player.play("sliding_start")
+			slide_request_timer.stop()
+			
+		State.SLIDING_LOOP:
+			animation_player.play("sliding_loop")
+			
+		State.SLIDING_END:
+			animation_player.play("sliding_end")
+		
+
+
 			
 #	if to == State.WALL_JUMP:
 #		Engine.time_scale = 0.3
